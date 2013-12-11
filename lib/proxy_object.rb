45559afd91a58ab4b9870bc1aeb630240f22110e
Piotr Solnica <piotr.solnica@gmail.com>
2013-12-11 01:51:49 +0100

    Extract methods into a module

--- lib/proxy_object.rb
+++ lib/proxy_object.rb
@@ -18,21 +18,32 @@ class ProxyObject < Module
 
   def initialize(name)
     attr_reader name
+    ivar = "@#{name}"
 
     define_method(:initialize) do |*args, &block|
-      instance_variable_set("@#{name}", args.first)
+      instance_variable_set(ivar, args.first)
       @__proxy_args = args[1..args.size]
     end
 
-    define_method(:method_missing) do |method_name, *args, &block|
-      target = send(name)
+    define_method(:__proxy_target__) do
+      instance_variable_get(ivar)
+    end
 
-      if target.respond_to?(method_name)
-        response = target.public_send(method_name, *args, &block)
+    include Methods
+  end
 
-        if response.equal?(target)
+  module Methods
+    def respond_to_missing?(method_name, include_all)
+      __proxy_target__.respond_to?(method_name, include_all)
+    end
+
+    def method_missing(method_name, *args, &block)
+      if __proxy_target__.respond_to?(method_name)
+        response = __proxy_target__.public_send(method_name, *args, &block)
+
+        if response.equal?(__proxy_target__)
           self
-        elsif response.kind_of?(target.class)
+        elsif response.kind_of?(__proxy_target__.class)
           self.class.new(*@__proxy_args.unshift(response))
         else
           response
@@ -40,10 +51,6 @@ class ProxyObject < Module
       else
         super(method_name, *args, &block)
       end
-    end
-
-    define_method(:respond_to_missing?) do |method_name, include_all|
-      send(name).respond_to?(method_name, include_all)
     end
   end
 end
